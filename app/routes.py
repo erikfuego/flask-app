adca3d0b84a5dd12db30b981a962e1a508c7dbb1
erikfuego <tuf98039@temple.edu>
2020-03-02 22:14:05 -0500

    completed chapter 3: web forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -7,8 +8,8 @@ def index():
     user = {'username': 'Erik'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'username': 'Ryan'},
+            'body': 'You\'re the GOAT!'
         },
         {
             'author': {'username': 'Susan'},
@@ -16,3 +17,12 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
