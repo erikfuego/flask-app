9c52865db41f03cfeb19c833a2dd796e08f831d0
erikfuego <tuf98039@temple.edu>
2020-04-26 13:12:39 -0400

    created a base User object for the db

---
+++ app/models.py
@@ -0,0 +1,17 @@
+"""
+Created on 4/26/2020
+
+@author: erosales
+
+Description of file: 
+"""
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}'.format(self.username)
