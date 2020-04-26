147a1e72cecf9908b1dc790b316e6cdc6e01a266
erikfuego <tuf98039@temple.edu>
2020-04-26 16:10:37 -0400

    created LoginManager object

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_login import LoginManager
+
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -6,8 +8,10 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = SQLAlchemy(app)
+login = LoginManager(app)
+login.login_view = 'login'
 
+db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
 from app import routes, models
